d3a5c13a3a492ac02820ac66ca9e5df957924342
Tomasz Kisiel <tkisiel5w4@yahoo.com>
2024-03-10 20:01:35 +0100

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM tensorflow/tensorflow:2.15.0-jupyter
+
+RUN pip install --upgrade scikit-learn
--- train.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it -v $(pwd)/app:/app -v $(pwd)/data:/data  tensorflow/tensorflow:latest python /app/main.py
